b4c6d7772d5b72252ab74bad6e8c10361637e408
Benzer Bett <benzerbett@gmail.com>
2020-01-30 16:45:42 +0300

    query params set wel

--- routes/county/dataquality.js
+++ routes/county/dataquality.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router()
-let {getApiDocs} = require('../../utils/index')
+let {getApiDocs, } = require('../../utils/index')
+let {fetchDefaults} = require('../../middleware/common')
 let {fetchCompleteness, fetchComparison, fetchConcordance, fetchConsistency} = require('../../middleware/county/dataquality')
 
 router.get('/', (req, res) => {
@@ -10,24 +11,40 @@ router.get('/', (req, res) => {
 
 router.get('/completeness/:ou?/:level?/:pe?', async (req, res) => {
     let {ou, level, pe } = req.params
+    let defaults = await fetchDefaults() 
+    if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
+    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(pe === undefined || pe === null || pe === " " || pe === '~'){level = defaults.period}
     let fetchedData = await fetchCompleteness(ou,level,pe) 
     res.json({ fetchedData});
 });
 
 router.get('/concordance/:ou?/:level?/:pe?', async (req, res) => {
     let {ou, level, pe } = req.params
+    let defaults = await fetchDefaults() 
+    if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
+    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(pe === undefined || pe === null || pe === " " || pe === '~'){level = defaults.period}
     let fetchedData = await fetchConcordance(ou,level,pe)
     res.json({ fetchedData});
 });
 
 router.get('/consistency/:ou?/:level?/:pe?', async (req, res) => {
     let {ou, level, pe } = req.params
+    let defaults = await fetchDefaults() 
+    if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
+    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(pe === undefined || pe === null || pe === " " || pe === '~'){level = defaults.period}
     let fetchedData = await fetchConsistency(ou,level,pe)
     res.json({ fetchedData});
 });
 
 router.get('/comparison/:ou?/:level?/:pe?', async (req, res) => {
     let {ou, level, pe } = req.params
+    let defaults = await fetchDefaults() 
+    if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
+    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(pe === undefined || pe === null || pe === " " || pe === '~'){level = defaults.period}
     let fetchedData = await fetchComparison(ou,level,pe)
     res.json({ fetchedData});
 });
