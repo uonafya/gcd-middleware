4b406951a2be13e6bb4ce159af5a6debe8aa8b16
Benzer Bett <benzerbett@gmail.com>
2020-04-29 15:29:22 +0300

    national endpoints

--- middleware/national/nationalsummary.js
+++ middleware/national/nationalsummary.js
@@ -14,10 +14,10 @@ let fetchnationalsummary = async (ou,level,pe) => {
     }
 }
 
-let fetchkemsasummary = async (pe) => {
-    let {url, default_org_unit, default_period} = endpoints.filter(ept => ept.id == "national__kemsa_summary")[0]
-    let defaults = {default_pe: default_period, default_ou: default_org_unit}
-    let query = {pe, ou}
+let fetchkemsasummary = async (ou,level,pe) => {
+    let {url, default_org_unit, default_org_unit_level, default_period} = endpoints.filter(ept => ept.id == "national__kemsa_summary")[0]
+    let defaults = {default_pe: default_period, default_ou: default_org_unit, default_lvl: default_org_unit_level}
+    let query = {pe, ou, level}
     try {
         let final_url = appendQueriesToUrl(url, query, defaults)
         let sc = await justFetch(final_url)
--- routes/national/nationalsummary.js
+++ routes/national/nationalsummary.js
@@ -1,16 +1,27 @@
 let express = require('express');
 let router = express.Router()
-let {fetchnationalsummary } = require('../../middleware/national/nationalsummary.js')
+let {fetchDefaults} = require('../../middleware/common')
+let {fetchnationalsummary, fetchkemsasummary } = require('../../middleware/national/nationalsummary.js')
 
-router.get(':ou?/:level?/:pe?', async (req, res) => { //TODO: set constant National OU
+router.get('/mos/:ou?/:level?/:pe?', async (req, res) => { //TODO: set constant National OU
     let {ou, level, pe } = req.params
+    let defaults = await fetchDefaults()
+    // let defaults = {pe: "LAST_MONTH", ou: "HfVjCurKxh2", level: "1"}
+    if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
+    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(pe === undefined || pe === null || pe === " " || pe === '~'){pe = defaults.period}
     let fetchedData = await fetchnationalsummary(ou,level,pe)
     res.json({ fetchedData});
 });
 
-router.get('kemsasummary/:ou?/:pe?', async (req, res) => { //TODO: set constant National OU
-    let {ou, pe } = req.params
-    let fetchedData = await fetchkemsasummary(ou,pe)
+router.get('/kemsasummary/:ou?/:level?/:pe?', async (req, res) => { //TODO: set constant National OU
+    let {ou, level, pe } = req.params
+    let defaults = await fetchDefaults()
+    // let defaults = {pe: "LAST_MONTH", ou: "HfVjCurKxh2", level: "1"}
+    if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
+    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(pe === undefined || pe === null || pe === " " || pe === '~'){pe = defaults.period}
+    let fetchedData = await fetchkemsasummary(ou,level,pe)
     res.json({ fetchedData});
 });
 
