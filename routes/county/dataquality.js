11d1f494f2e148bdd3b0b812020bcf932782e3f7
Benzer Bett <benzerbett@gmail.com>
2020-07-10 13:25:28 +0300

    & comparison too

--- routes/county/dataquality.js
+++ routes/county/dataquality.js
@@ -13,7 +13,7 @@ router.get('/completeness/:ou?/:level?/:pe?', async (req, res) => {
     let {ou, level, pe } = req.params
     let defaults = await fetchDefaults() 
     if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
-    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(level === undefined || level === null || level === " " || level === '~'){level = 5 //defaults.level}
     if(pe === undefined || pe === null || pe === " " || pe === '~'){pe = defaults.period}
     let prapplo = req.app.locals.program; let prog = req.query.program || prapplo
     let fetchedData = await fetchCompleteness(ou,level,pe,prog) 
