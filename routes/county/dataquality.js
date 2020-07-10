323c0dcef28a206c9e6a9082773bafbe48b5bed6
Benzer Bett <benzerbett@gmail.com>
2020-07-10 13:23:33 +0300

    default level=5 in dq:completeness

--- routes/county/dataquality.js
+++ routes/county/dataquality.js
@@ -46,7 +46,7 @@ router.get('/comparison/:ou?/:level?/:pe?', async (req, res) => {
     let {ou, level, pe } = req.params
     let defaults = await fetchDefaults() 
     if(ou === undefined || ou === null || ou === " " || ou === '~'){ou = defaults.dataViewOrganisationUnits[0].id}
-    if(level === undefined || level === null || level === " " || level === '~'){level = defaults.level}
+    if(level === undefined || level === null || level === " " || level === '~'){level = 5//defaults.level}
     if(pe === undefined || pe === null || pe === " " || pe === '~'){pe = defaults.period}
     let prapplo = req.app.locals.program; let prog = req.query.program || prapplo
     let fetchedData = await fetchComparison(ou,level,pe,prog)
