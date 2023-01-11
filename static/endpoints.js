ca6c3a03bea9d06a4aff6746c1c494203fa58896
faithchep <faithchepkoech40@gmail.com>
2023-01-11 11:33:34 +0300

    fix risk parameters

--- static/endpoints.js
+++ static/endpoints.js
@@ -448,7 +448,31 @@ let endpoints = [
     "default_org_unit": "HfVjCurKxh2",
     "default_org_unit_level": "",
     "Notes": ""
-  }
+  },
+  {
+    page: 'Risk Parameters',
+    level: 'County',
+    name: 'Risk One',
+    id: 'county__risk_parameters_risk_1',
+    local_url: `${APP_BASE_URL}/api/county/riskparameters/f0AIAR5pJ2F`,
+    url: `${DHIS_BASE_API_URL}/analytics.json?dimension=dx:f0AIAR5pJ2F.w77uMi1KzOH`,
+    default_period: 'LAST_3_MONTHS',
+    default_org_unit: 'HfVjCurKxh2',
+    default_org_unit_level: 5,
+    Notes: ''
+  },
+  {
+    page: 'Risk Parameters',
+    level: 'County',
+    name: 'Risk Two',
+    id: 'county__risk_parameters_Risk_2',
+    local_url: `${APP_BASE_URL}/api/county/riskparameters/rqzfl66VFyd`,
+    url: `${DHIS_BASE_API_URL}/analytics.json?dimension=dx:f0AIAR5pJ2F.HWtHCLAwprR;f0AIAR5pJ2F.rqzfl66VFyd;f0AIAR5pJ2F.rPAsF4cpNxm;f0AIAR5pJ2F.yuvCdaFqdCW;f0AIAR5pJ2F.w77uMi1KzOH;f0AIAR5pJ2F.unVIt2C0cdW;f0AIAR5pJ2F.CckV73xy6HB;RRnz4uPHXdl.REPORTING_RATE&displayProperty=NAME`,
+    default_period: 'LAST_3_MONTHS',
+    default_org_unit: 'HfVjCurKxh2',
+    default_org_unit_level: 5,
+    Notes: ''
+  },
 ]
 
 module.exports = endpoints
